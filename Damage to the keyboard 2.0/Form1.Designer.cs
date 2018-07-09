9d7a68d02292da58ed20ce73fffde0b4df5e77aa
Codethm <dong55.102375@gmail.com>
2018-07-09 22:07:10 +0700

    add all files
    2.0/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs

---
+++ Damage to the keyboard 2.0/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Damage_to_the_keyboard_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "key/sec";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(327, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(327, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Count";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(64, 438);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(146, 60);
+            this.panel1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label4.Location = new System.Drawing.Point(226, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 78);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "เหลือเวลา\r\n\r\nจำนวนกด";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Title = "Sec";
+            chartArea1.AxisY.Title = "Key";
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 104);
+            this.chart1.Name = "chart1";
+            series1.BackImageTransparentColor = System.Drawing.Color.White;
+            series1.BackSecondaryColor = System.Drawing.Color.White;
+            series1.BorderColor = System.Drawing.Color.Green;
+            series1.BorderWidth = 4;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.LabelBackColor = System.Drawing.Color.Transparent;
+            series1.LabelBorderColor = System.Drawing.Color.White;
+            series1.LabelForeColor = System.Drawing.Color.Beige;
+            series1.Legend = "Legend1";
+            series1.Name = "AVG.";
+            series2.BorderWidth = 4;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Key/Sec";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(805, 259);
+            this.chart1.TabIndex = 7;
+            this.chart1.TabStop = false;
+            this.chart1.Text = "chart1";
+            this.chart1.TextAntiAliasingQuality = System.Windows.Forms.DataVisualization.Charting.TextAntiAliasingQuality.Normal;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.Location = new System.Drawing.Point(716, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "F5 re-start";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 573);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "I hate this keyboard";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.start);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Damage to the keyboard 2.0/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Damage_to_the_keyboard_2._0
+{
+    public partial class Form1 : Form
+    {
+        decimal Time = 0;
+        decimal avg = 0;
+        int count = 0;
+        Timer T = new Timer();
+        Timer Col = new Timer();
+        int keypersec = 0;
+        Timer sec = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            T.Interval = 90;
+            Col.Interval = 45;
+            sec.Interval = 900;
+            T.Tick += T_Tick;
+            Col.Tick += Col_Tick;
+            sec.Tick += Sec_Tick;
+        }
+
+        private void start(object sender, KeyEventArgs e)
+        {
+            label5.Hide();
+            sec.Start();
+            T.Start();
+            chart1.Series["Key/Sec"].Points.AddXY(0.05, 0);
+            chart1.Series["AVG."].Points.AddXY(0.05, 0);
+
+            this.KeyDown -= start;
+            this.KeyDown += GO;
+
+        }
+        private void GO(object sender, KeyEventArgs e)
+        {
+            count++;
+            keypersec++;
+            this.BackColor = Color.Orange;
+            Col.Start();
+        }
+        private void End(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                Time = 0;
+                avg = 0;
+                count = 0;
+                label2.Text = 30.ToString();
+                label3.Text = 0.ToString();
+                label1.Text = 0.ToString();
+                panel1.Width = 40;
+                foreach (var series in chart1.Series)
+                {
+                    series.Points.Clear();
+                }
+                this.KeyDown -= End;
+                this.KeyDown += start;
+            }
+        }
+
+
+        private void Sec_Tick(object sender, EventArgs e)
+        {
+            chart1.Series["Key/Sec"].Points.AddXY(Time, keypersec);
+            keypersec = 0;
+        }
+        private void T_Tick(object sender, EventArgs e)
+        {
+            Time += 0.1m;
+            avg = count / (Time);
+
+            label1.Text = avg.ToString();
+            label2.Text = (30 - Time).ToString();
+            label3.Text = count.ToString();
+
+            chart1.Series["AVG."].Points.AddXY(Time, avg);
+
+            panel1.Width = (int)(avg * 15);
+            if (avg > 35)
+                panel1.BackColor = Color.Red;
+            else if (avg > 27)
+                panel1.BackColor = Color.Yellow;
+            else if (avg > 22)
+                panel1.BackColor = Color.GreenYellow;
+            else if (avg > 10)
+                panel1.BackColor = Color.Green;
+            if ((30 - Time) == 0)
+            {
+                label5.Show();
+                T.Stop();
+                sec.Stop();
+                this.KeyDown += End;
+                this.KeyDown -= GO;
+            }
+        }
+        private void Col_Tick(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+            Col.Stop();
+        }
+    }
+}
